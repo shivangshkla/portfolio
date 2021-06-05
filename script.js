7209090cc40d5a29e62121190cb44d90d9db5534
Shivang Shukla <shuklashivang036@gmail.com>
2021-06-04 22:59:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const button = document.querySelector('.button');
 const info = document.querySelector('.info');
+const background = document.querySelector('.about');
 function come(){
     info.setAttribute('style','animation-name:motion-in;animation-duration:1s;animation-fill-mode:forwards;');
     button.setAttribute('style','animation-name:motion-in;animation-duration:1.1s;animation-fill-mode:forwards;');
 }
-window.onload = come();
+background.style.backgroundImage.onload = come();

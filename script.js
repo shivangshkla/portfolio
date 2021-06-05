4727f74ef8e2acd41018390832379cbbb347bcda
Shivang Shukla <shuklashivang036@gmail.com>
2021-06-05 10:30:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const about = document.querySelector('.about');
-const info = document.querySelector('.info');
-console.log("1");
-come();
-function come(){
-    info.setAttribute('style','animation-name:motion-in;animation-duration:1s;animation-fill-mode:forwards;');
-}

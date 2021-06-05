1ec1e0c58b23d43297e7b61e216694f2a8f1ca28
Shivang Shukla <shuklashivang036@gmail.com>
2021-06-04 22:57:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ function come(){
     info.setAttribute('style','animation-name:motion-in;animation-duration:1s;animation-fill-mode:forwards;');
     button.setAttribute('style','animation-name:motion-in;animation-duration:1.1s;animation-fill-mode:forwards;');
 }
-background.style.background.onload = come();
+Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+    console.log('images finished loading');
+    come();
+});

683e330a8465d9918a0ca4863cc71244a16694b0
Shivang Shukla <shuklashivang036@gmail.com>
2021-06-12 10:51:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const navbar = document.querySelector('header');
+const menuText1 = document.querySelector('.nav-links-1');
+const menuText2 = document.querySelector('.nav-links-2');
+const menuText3= document.querySelector('.nav-links-3');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+        menuText1.classList.add('nav-after');
+        menuText2.classList.add('nav-after');
+        menuText3.classList.add('nav-after');
+    } else {
+        navbar.classList.remove('nav-active');
+        menuText1.classList.remove('nav-after');
+        menuText2.classList.remove('nav-after');
+        menuText3.classList.remove('nav-after');
+    }
+};
+
